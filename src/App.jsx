fd342f400c77cbb327a9c0083d319dc3968958fd
Danie Antón <danielantcam@gmail.com>
2025-07-11 13:27:49 +0200

    Added an academic roadmap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Stack from "./components/Stack";
 import ProjectsGrid from "./components/ProjectsGrid";
+import Roadmap from "./components/RoadmapBlock";
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -8,6 +9,23 @@ export default function App() {
     <Header />
     <Stack />
     <ProjectsGrid />
+    <Roadmap events={[
+      {
+        title: "React Tutorial",
+        description: "Completed 'React Tutorial' by Jad Joubran, an interactive course that introduces React fundamentals through hands-on coding challenges and real-world projects, ideal for building a solid foundation in modern front-end development.",
+        date: "June 2025"
+      },
+      {
+        title: "Computer Science",
+        description: "Started a Bachelor's Computer Science degree in the Autonomous University of Madrid.",
+        date: "September 2024"
+      },
+      {
+        title: "Learn JavaScript",
+        description: "Took 'Learn JavaScript' by Jad Joubran, a complete course on JavaScript that covers all the way from the basics to more advanced concepts that are reinforced through real-life projects.",
+        date: "May 2024"
+      }
+    ]} />
     <Footer />
   </>);
 }
---
+++ src/components/RoadmapBlock.jsx
@@ -0,0 +1,41 @@
+export default function Roadmap({ events }){
+    return <article role="list">
+        <h3 className="
+            text-white font-semibold text-3xl mb-8
+            lg:text-5xl
+        ">
+            Academic Roadmap
+        </h3>
+        {events.map((event, index)=> <RoadmapBlock event={event} key={index}/>)}
+    </article>;
+}
+
+function  RoadmapBlock({ event }){
+    const { title, description, date } = event;
+
+    return <section role="listitem" className="flex mb-2">
+        <div aria-hidden="true" className="flex flex-col items-center mr-6">
+            <div className="w-6 h-6 bg-blue-500 rounded-full mb-1"></div>
+            <div className=" grow w-2 bg-blue-500 rounded-sm"></div>
+        </div>
+        <div>
+            <div className="
+                flex flex-col mb-1
+                sm:flex-row sm:items-center sm:h-6 sm:gap-4 sm:mb-0
+            ">
+                <h4 className="
+                    text-white font-semibold text-md
+                    sm:text-xl
+                ">{title}</h4>
+                <span className="
+                    text-gray-500 font-semibold text-xs
+                    sm:text-base
+                ">{date}</span>
+            </div>
+            <p className="
+                text-gray-400 font-semibold pb-4 text-sm
+                sm:text-base
+            ">{description}</p>
+        </div>
+    </section>;
+}
