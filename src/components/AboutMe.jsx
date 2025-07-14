f93d34368df0fbb06491230cf8d5ee65dd4a15da
Danie Antón <danielantcam@gmail.com>
2025-07-14 12:46:24 +0200

    Minor style change

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,11 +11,11 @@ export default function AboutMe(){
         </h3>
         <p className="text-white mb-4">Hey, I am <strong>Daniel Antón Camarero</strong>, a 19-year-old <strong>Computer Science</strong> student and a selft-taught <strong>Fullstack Web Developer</strong> from Burgos, Spain. However, I'm currently studying in the Autonomous University of Madrid.</p>
 
-        <p className="text-white mb-4">I began my journey as a developer driven by the desire to build, my own projects and soon discovered a real passion for it.</p>
+        <p className="text-white mb-4">I began my journey as a developer driven by the desire to build my own projects and soon discovered a real passion for it.</p>
 
         <h4 className="text-white text-xl font-semibold mb-2">Soft skills</h4>
         <ul className="
-            list-disc list-inside  grid grid-cols-1
+            list-disc list-inside  grid grid-cols-1 marker:text-blue-500
             md:grid-cols-2
             lg:grid-cols-2
             xl:grid-cols-1
